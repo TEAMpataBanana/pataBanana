921bf0d4779d785c34975d009edccf9ac7e2be0d
eulachii <eula.montilla@jmc.edu.ph>
2017-01-11 10:20:25 +0800

    first commit
    :confetti_ball: Added .gitattributes

---
+++ fuel/app/config/crypt.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  'crypto_key' => 'M_oMAUGBgP6EYUI0ysUgsgso',
+  'crypto_iv' => 'tF0O6ExgMVPQgSsYpAE4MJjg',
+  'crypto_hmac' => '_KMQDYUGMjAAcMEcPQmLQKjs',
+);
---
+++ fuel/app/logs/2017/01/10.php
@@ -0,0 +1,19 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2017-01-10 07:59:02 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 07:59:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:00:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:00:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:10:22 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:11:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:12:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:12:54 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:13:46 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:14:06 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:18:29 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:18:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:18:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:19:09 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:19:10 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:19:16 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2017-01-10 08:19:20 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
